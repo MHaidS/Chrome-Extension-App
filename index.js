8081279072c6dc59cf616b9466f09d23f3c0502c
Reese-haids <reese.yes@gmail.com>
2022-12-09 21:51:53 +0800

    third commit

--- index.js
+++ index.js
@@ -1,60 +1,52 @@
-let myLeads = ["www.google.com", "www.yahoo.com", "www.apple.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+// 36. Get the leads from localStorage
+// 36.1) Get the leads from the localStorage - PS: JSON.parse()
+//        JSON.parse(localstorage.getItem("myLeads"));
+// 36.2) Store it in a variable, leadsFromLocalStorage
 
-// 33. Your first localStorage
-// 1. Save a key-value pair in localStorage ===>
-// localStorage.setItem("myName", "MHaids");
-// 2. Refresh the page. Get the value and log it to the console ===>
-// let name1 = localStorage.getItem("myName");
-// console.log(name1);
-// CONSOLE: name1 ==> MHaids
-// 3. Clear localStorage ===>
 // localStorage.clear();
-// 4. Log out the name after clearing out the storage & this will now return 'null' ===>
-let name1 = localStorage.getItem("myName");
-console.log(name1);
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// console.log(leadsFromLocalStorage);
 
-// HINTS:
-// localStorage.setItem(key, value)
-// localStorage.getItem(key)
-// localStorage.clear()
-// PS: both key and value need to be strings
+// 39. Checking localStorage before rendering
+// 39.1) Check if leadsFromLocalStorage is truthy
+// 39.2) If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+// if 'leadsFromLocalStorage' is a truthy value, it will run renderLeads(); that is, we have persisted our leads across refresh; else, it will move on to the nxt line
 
-// 21. Create the render function
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  // console.log(myLeads);
-  // 21.2) Call the renderLeads() function
-  // 22. Clear the input field
   inputEl.value = "";
+  // 35. Save the leads to localStorage
+  // Save the myLeads array to localStorage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
   renderLeads();
+
+  // To verify that it works:
+  console.log(localStorage.getItem("myLeads"));
+  // OUTPUT:
+  // myLeads
+  // ['https://www.apple.com']
 });
 
-// 20.1) Create a variable, listItems, to hold all the HTML for the list items
-// 21.1) Wrap the code below in a renderLeads() function
 function renderLeads() {
   let listItems = "";
-
   for (let i = 0; i < myLeads.length; i++) {
-    // 20.2) Add the item to the listItems variable instead of the ulEl.innerHTML
-    // 23. Add the <a> tag
-    // Wrap the lead in an anchor tag (<a>) inside the <li>
-    // 24. Template strings
-    // 28) Refactor the code below to use a template string
     listItems += `
-      <li>
-        <a target="_blank" href="${myLeads[i]}">
-          ${myLeads[i]} 
-        </a>
-      </li>
-    `;
-    // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
-    // listItems += `<li><a href="#" target="_blank">${myLeads[i]} </a></li>`;
-
-    // ulEl.innerHTML += `<li>${myLeads[i]} </li>`;
+    <li>
+      <a target="_blank" href='${myLeads[i]}'>
+        ${myLeads[i]}
+      </a>    
+    </li>
+  `;
   }
-  // 20.3) Render the listItems inside the unordered list using ulEl.innerHTML
   ulEl.innerHTML = listItems;
 }
